0e1cc1973aeafe53305aef538607294ad4153e97
Quakerhash <srm0673@gmail.com>
2025-04-22 19:12:07 +0530

    Update settings.py

--- temp/settings.py
+++ temp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lv=)kz%t(3t&n$wucts5zjujn(3)t!3kn#qd7gst=rqk!#&^86'
+SECRET_KEY =os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
