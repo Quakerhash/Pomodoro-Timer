28ca377ea440bb369f19a1e0919b95cfa669c499
Quakerhash <srm0673@gmail.com>
2025-04-22 21:50:42 +0530

    Update views.py

--- temp/views.py
+++ temp/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render, redirect
 def HOME(request):
     return render(request, 'home.html')
 def ABOUT(request):
-    return render(request, 'About.html')
+    return render(request, 'about.html')
 def ss(request):
     return render(request, 'Studys.html')
 def Timer(request):
     return render(request, 'Timer.html')
 def Progress(request):
-    return render(request, 'Progress.html')
+    return render(request, 'progress.html')
